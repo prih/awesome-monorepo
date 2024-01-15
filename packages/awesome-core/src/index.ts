72150c1307b61b46e780578118bcbd0c0bc7a408
prih <prih@prihhost.com>
2024-01-15 18:25:20 +0000

    v0.2

--- packages/awesome-core/src/index.ts
+++ packages/awesome-core/src/index.ts
@@ -1,4 +1,4 @@
-import {add, sub} from '@prih/awesome-utils';
+import {add, div, mul, sub} from '@prih/awesome-utils';
 
 export class AwesomeCore {
   plus(a: number, b: number): number {
@@ -7,5 +7,13 @@ export class AwesomeCore {
 
   minus(a: number, b: number): number {
     return sub(a, b);
+  }
+
+  times(a: number, b: number): number {
+    return mul(a, b);
+  }
+
+  div(a: number, b: number): number {
+    return div(a, b);
   }
 }
